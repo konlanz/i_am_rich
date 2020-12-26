e05420bc69c683044011b1ac26f5bd90b971fff0
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2020-12-26 20:44:41 +0000

    this is an initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
