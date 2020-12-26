4c28bc7d0425614ae7f9a82a3511d57860f5ebef
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2020-12-26 21:46:14 +0000

    this is an initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("I am rich"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-      ),
-    ),
-  );
+          appBar: AppBar(
+            title: Text('I am Rich'),
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+          backgroundColor: Colors.lightBlueAccent,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ))));
 }
